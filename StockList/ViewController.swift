0f6b6c976dbf97f0fa2a1d356d9d308bf5c4ea89
KP <kdp119@scarletmail.rutgers.edu>
2021-10-29 10:56:13 -0400

    Initial Commit

---
+++ StockList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockList
+//
+//  Created by Krish Pathak on 10/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
