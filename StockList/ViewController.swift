b51aca2ac719e8778ade2333a5081b21096da0cb
KP <kdp119@scarletmail.rutgers.edu>
2021-11-02 10:59:14 -0400

    SL

--- StockList/ViewController.swift
+++ StockList/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func uiStock(_ sender: UIButton) {
+    }
+    @IBAction func uiCrypto(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
